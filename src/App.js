be54eaaa8095525448f57d1fe5f1a3cd389d6556
midCenturyDude7 <mgriffes@gmail.com>
2022-02-23 09:45:33 -0800

    Used object spread syntax to complete lesson 1.2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from 'react';
-// import Header from './components/Header';
+import Header from './components/Header';
 import Content from './components/Content';
-// import Total from './components/Total';
+import Total from './components/Total';
 
 const App = () => {
-  // const course = 'Half Stack application development';
-  // const part1 = 'Fundamentals of React';
-  // const exercises1 = 10;
-  // const part2 = 'Using props to pass data';
-  // const exercises2 = 7;
-  // const part3 = 'State of a component';
-  // const exercises3 = 14;
-  // const totals = exercises1 + exercises2 + exercises3;
+  const course = 'Half Stack application development';
+  const exercises1 = 10;
+  const exercises2 = 7;
+  const exercises3 = 14;
+  const totals = exercises1 + exercises2 + exercises3;
 
   return (
     <div>
+      <Header course={course} />
       <Content />
+      <Total totals={totals} />
     </div>
   );
 }
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,13 +1,35 @@
 import React from "react";
-import Parts from "./Parts";
-
 
 const Content = () => {
 
     return (
+        <div>
+            <Part {...Part.parts} />
+        </div>        
+    );
+}
+
+const Part = () => {
+    const parts = {
+        part1: {
+            name: "Fundamentals of React",
+            exercises: 10 
+        },
+        part2: {
+            name: "Using props to pass data",
+            exercises: 7
+        },
+        part3: {
+            name: "State of a component",
+            exercises: 14
+        }
+    }
+    return (
         <>
-            <Parts />
-        </>        
+            <p>{parts.part1.name + ": " + parts.part1.exercises}</p>
+            <p>{parts.part2.name + ": " + parts.part2.exercises}</p>
+            <p>{parts.part3.name + ": " + parts.part3.exercises}</p>
+        </>
     );
 }
 
