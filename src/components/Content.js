8d21cea68382b7e52fdf49693b0b3c39529e3e86
midCenturyDude7 <mgriffes@gmail.com>
2022-02-22 22:09:34 -0800

    First effort solving exercise 1.2

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Parts from "./Parts";
 
-const Content = (props) => {
+
+const Content = () => {
+
     return (
         <>
-            <p>{props.part1}:<span>{props.exercises1}</span></p>
-            <p>{props.part2}:<span>{props.exercises2}</span></p>  
-            <p>{props.part3}:<span>{props.exercises3}</span></p> 
+            <Parts />
         </>        
     );
 }
