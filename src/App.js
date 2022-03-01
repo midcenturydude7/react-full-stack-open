3b41a5cfba9e99b18a63778a450d39726a197289
midCenturyDude7 <mgriffes@gmail.com>
2022-03-01 08:19:35 -0800

    Working with stateful components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 import React from 'react';
-import Header from './components/Header';
-import Content from './components/Content';
-import Total from './components/Total';
+import Counter from './components/Counter';
+// { Getting started }
+// import Header from './components/Header';
+// import Content from './components/Content';
+// import Total from './components/Total';
+
+// { Component helper functions and destructuring }
+// import Hello from './components/Hello';
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const exercises1 = 10;
-  const exercises2 = 7;
-  const exercises3 = 14;
-  const totals = exercises1 + exercises2 + exercises3;
-
   return (
-    <div>
-      <Header course={course} />
-      <Content />
-      <Total totals={totals} />
-    </div>
+    <Counter />
   );
+  // const {counter} = props
+  
+  // return (
+  //   <div>{counter}</div>
+  // )
+
+  // { Getting started }
+  // const course = 'Half Stack application development';
+  // const exercises1 = 10;
+  // const exercises2 = 7;
+  // const exercises3 = 14;
+  // const totals = exercises1 + exercises2 + exercises3;
+
+  // return (
+  //   <div>
+  //     <Header course={course} />
+  //     <Content />
+  //     <Total totals={totals} />
+  //   </div>
+
+// { Component helper functions and destructuring }
+//   const name = "Peter";
+//   const age = 10;
+
+//   return (
+//     <div>
+//       <h1>Greetings</h1>
+//       <Hello name="Henry" age={14} />
+//       <Hello name={name} age={age} />
+//     </div>
+//   );
+// }
 }
 
 export default App;
