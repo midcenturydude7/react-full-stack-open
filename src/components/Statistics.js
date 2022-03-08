7259a51570deb399d74a4e4d0c012e39d93170db
midCenturyDude7 <mgriffes@gmail.com>
2022-03-08 08:59:44 -0800

    Used conditional w/in Statistics component to complete exercise

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -2,13 +2,21 @@ import React from "react";
 
 const Statistics = ({total, average, positive}) => {
 
-    return (
-        <>
-            <span>All: {total}</span><br />
-            <span>Average: {average}</span><br />
-            <span>Positive: {positive}%</span><br />
-        </>
-    )
+    if (total === 0) {
+        return (
+            <>
+                <h2>No Feedback Given</h2>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <span>All: {total}</span><br />
+                <span>Average: {average}</span><br />
+                <span>Positive: {positive}%</span><br />
+            </>
+        )
+    }
 }
 
 export default Statistics;
