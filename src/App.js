92e938bf62d54a8dd2e9d9527ba427c198a16abc
midCenturyDude7 <mgriffes@gmail.com>
2022-03-16 14:29:14 -0700

    Working with state to add items to a list

--- src/App.js
+++ src/App.js
@@ -1,65 +1,46 @@
 import React from 'react';
-import Note from './components/Notes';
 
-const App = (props) => {
-  // Initialized the piece of state stored in the notes array (in index.js) with the array of notes passed in the props parameter
-  const [notes, setNotes] = React.useState(props.notes)
-
-  // Stores the user-submitted input and the initial state is set as of the input element's value attribute
-  const [newNote, setNewNote] = React.useState("A new note...")
-
-  // Keeps track of which notes should be displayed based on the 'important' property
-  const [showAll, setShowAll] = React.useState(true)
-
-  // For adding new notes to the form tag
-  const addNote = (event) => {
-    event.preventDefault()
-    // console.log("Button clicked", event.target)
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1,
+const App = () => {
+  const [persons, setPersons] = React.useState([
+    {
+      id: 1,
+      name: "Arto Hellas"
+    },
+    {
+      id: 2,
+      name: "Fred Flinstone"
     }
-
-    setNotes(notes.concat(noteObject))
-    setNewNote("")
-  }
-
-  // Event handler to the form element that is called when the form is submitted
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
-
-  // Function to handle which notes to show
-  const notesToShow = showAll
-    ? notes
-    : notes.filter((note) => note.important === true)
+  ])
 
   return (
     <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          Show {showAll ? "Important" : "All"}
-        </button>
-      </div>
-      <ul>
-        {notesToShow.map((note) => 
-          <Note key={note.id} note={note} />
-          )}
-      </ul>
-      <form onSubmit={addNote}>
-        <input 
-          value={newNote} 
-          onChange={handleNoteChange} 
-        />
-        <button type="Submit">Save</button>
-      </form>
+      <h1>Current names on the list:</h1>
+      <CurrentList persons={persons} />
+      <h1> Add a name:</h1>
+      <NewList persons={persons} />
     </div>
   )
+}
 
+const CurrentList = ({ persons }) => {
+  const nameId = persons.map((person) => person.id)
+  const nameOnList = persons.map((person) => person.name)
+  
+  return (
+    <>
+      <p>{nameId[0]}. {nameOnList[0]}</p>
+      <p>{nameId[1]}. {nameOnList[1]}</p>
+    </>
+  )
+}
+
+const NewList = ({ persons }) => {
+  return (
+    <form>
+      <input field="text" placeholder="Enter a name"/>
+      <button>Add</button>
+    </form>
+  )
 }
 
 export default App;
