68633479ed7ffb6edce6d205c40248465229fa47
midCenturyDude7 <mgriffes@gmail.com>
2022-03-19 18:37:23 -0700

    Working with state to avoid adding duplicate names

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ const App = () => {
   // State that will store user submitted data and the initial state is an empty string
   const [newName, setNewName] = React.useState("");
 
+  const [includeName, setIncludeName] = React.useState(true)
+
   // Adding new names to the form tag  
   const addNewName = (event) => {
     event.preventDefault();
@@ -24,7 +26,7 @@ const App = () => {
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
- 
+
   // const nameExists = persons.map((person) => person.name === newName) ? alert("That name already exists in the phonebook! Try again") : (persons.map((person, i) =>
   //     <li key={i}>{person.name}</li>))
 
@@ -34,11 +36,21 @@ const App = () => {
         <li key={i}>{person.name}</li>
       )}
     </ol>
-
+  
   const grabName = persons.map((person) => person.name)
   console.log(grabName)
-    
-  const nameCheck = !grabName.includes(`${newName}`) ? addNameToList : alert("That name already exists in the phonebook! Try again")
+  
+  let namesList = grabName
+        
+  let uniqueNames = namesList.filter((person, index) => {
+    return namesList.indexOf(person) === index;
+    });
+    console.log(uniqueNames)
+  
+  const namesToInclude = !includeName ? grabName.includes(person => person.name === `${newName}`) : addNameToList
+
+
+  // const nameCheck = grabName.includes((person) => person.name === `${newName}`) ? removeDuplicate(grabName) && alert("Try another name!") : addNameToList
 
   return (
     <div>
@@ -50,13 +62,14 @@ const App = () => {
         <div>
           <button 
             type="submit"
+            onClick={() => setIncludeName(namesToInclude)}
           >
               Add
           </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>{nameCheck}</div>
+      <div>{addNameToList}</div>
     </div>
   )
 }
