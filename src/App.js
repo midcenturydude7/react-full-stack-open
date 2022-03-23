7973057201752fd6c6f45dae0068aad96434ae0c
midCenturyDude7 <mgriffes@gmail.com>
2022-03-22 20:06:54 -0700

    Uitilized new Set() to limit duplicates

--- src/App.js
+++ src/App.js
@@ -1,51 +1,32 @@
 import React from 'react';
-import PhoneBook from './components/PhoneBook';
-import Form from './Form';
 
 const App = () => {
-  // initalizes state with the name stored as Arto
-  const [persons, setPersons] = React.useState([
-    {
-      name: "Arto Hellas",
-    }
-  ])
-  // State that will store user submitted data and sets the initial state is an empty string
+  const [persons, setPersons] = React.useState(new Set());
   const [newName, setNewName] = React.useState("");
 
   const addNewName = (event) => {
-      event.preventDefault();
-      const nameObject = {
-          name: newName
-      }    
-      setPersons(persons.concat(nameObject));
-      setNewName("");
-  }
+    event.preventDefault();
+    if (newName.trim()) {
+      setPersons(prevState => new Set(prevState).add(newName.trim()))
+    }; 
+  };    
 
-  const handleNameChange = (event) => {
-      setNewName(event.target.value)
-  }
-  // State checks for duplicate names in the phonebook
-  // const [includeName, setIncludeName] = React.useState(true)
-  
-  // const grabName = persons.map((person) => person.name)
-  // console.log(grabName)
-  
-  // let namesList = grabName
-        
-  // let uniqueNames = namesList.filter((person, index) => {
-  //   return namesList.indexOf(person) === index;
-  //   });
-  //   console.log(uniqueNames)
-  
-  // const nameCheck = grabName.includes((person) => person.name === `${newName}`) ? removeDuplicate(grabName) && alert("Try another name!") : addNameToList
+  console.log(persons)
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Form addNewName={addNewName} handleNameChange={handleNameChange} />
-      <PhoneBook persons={persons}/>
+      <form onSubmit={addNewName}>
+        <input 
+          onChange={event => setNewName(event.target.value)}
+          value={newName}
+        />{" "}
+        <button type="submit">Add</button>
+        <h2>Numbers</h2>
+        <ul>{[...persons].map(event => <li key={event}>{event}</li>)}</ul>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default App;
